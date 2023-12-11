2a8a683920f04486ade7bfb41ab67e8e1663a7a0
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-11 16:21:41 +0900

    feat:위젯 겹치기,Align,Stack,Positioned

---
+++ Stacking Widgets/android/app/src/main/kotlin/hwibin/prac/stacking_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package hwibin.prac.stacking_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Stacking Widgets/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Widget 겹치기'),
+        ),
+        body: Body(),
+      ),
+    ),
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(150),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 280,
+            height: 280,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(140),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Count 0',
+            style: TextStyle(color: Colors.red, fontSize: 32),
+          ),
+        )
+      ],
+    );
+  }
+}
